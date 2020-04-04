e7f15be004e5b945831889eb0555028eb8536958
Ramiro Estrella <ramiro.estrellac@gmail.com>
2020-04-04 22:57:52 +0200

    Update Stops.js

--- screens/users/Stops.js
+++ screens/users/Stops.js
@@ -14,22 +14,47 @@ import axios from "axios";
 import { AsyncStorage } from "react-native";
 import { sanitizeString, getBadgeColor } from "../../helpers";
 import Loading from "../../common/components/Loading";
+import utmObj from "utm-latlng";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import getDistance from "geolib/es/getDistance";
 
 class StopsScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      stops: null
+      stops: null,
+      location: null
     };
     this.gotoDetail = this.gotoDetail.bind(this);
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    await this.getLocation();
     var self = this;
     axios
       .get("https://apunterd.com/paradas.json")
       .then(function (response) {
-        self.setState({ stops: response.data.features });
+        let stops = response.data.features;
+        let utm = new utmObj();
+        stops.map(stop => {
+          let points = utm.convertUtmToLatLng(
+            stop.geometry.coordinates[0],
+            stop.geometry.coordinates[1],
+            30,
+            "S"
+          );
+          let currentLocation = {
+            lat: self.state.location.coords.latitude,
+            lon: self.state.location.coords.longitude
+          };
+          const distance = getDistance(currentLocation, {
+            lat: Number(points.lat),
+            lon: Number(points.lng)
+          });
+          stop.distance = distance;
+        });
+        self.setState({ stops });
       })
       .catch(function (error) {
         console.log(error);
@@ -45,11 +70,62 @@ class StopsScreen extends Component {
     });
   }
 
+  getLocation = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    const deltas = this.calculateDeltas();
+    let location;
+    if (status === "granted") {
+      location = await Location.getCurrentPositionAsync({});
+    } else {
+      location = {
+        coord: {
+          latitude: 37.78825,
+          longitude: -122.4324
+        }
+      };
+    }
+    await this.setState({ location: { ...location, deltas } });
+  };
+
+  calculateDeltas() {
+    const latitudeDelta = 0.004757;
+    const longitudeDelta = 0.006866;
+    const coef = latitudeDelta / longitudeDelta;
+    const longitudeDeltaCalculated = this.calcLongitudeDelta(15.2);
+    const latitudeDeltaCalculated = longitudeDeltaCalculated * coef;
+    return {
+      longitudeDelta: longitudeDeltaCalculated,
+      latitudeDelta: latitudeDeltaCalculated
+    };
+  }
+
+  calcZoom(longitudeDelta) {
+    return Math.log(360 / longitudeDelta) / Math.LN2;
+  }
+
+  calcLongitudeDelta(zoom) {
+    var power = Math.log2(360) - zoom;
+    return Math.pow(2, power);
+  }
+
   renderLoading() {
     return (
       <Loading />
     )
   }
+
+  getDistanceUnit(distance) {
+    let unit = "m";
+    const kms = (distance / 1000).toFixed(2);
+    if(kms >= 1) {
+        distance = kms;
+        unit = "km";
+    }
+    return {
+        unit,
+        distance
+    }
+}
 
   renderContent() {
     const { stops } = this.state;
@@ -120,6 +196,16 @@ class StopsScreen extends Component {
                   >
                     <Text>{stop.properties.free}</Text>
                   </Badge>
+                  <MaterialIcons
+                name="place"
+                style={{
+                  fontSize: 20,
+                  marginTop: 10,
+                  marginLeft: 20,
+                  color: "#5e92f3"
+                }}
+              />
+                <Text style={{ marginTop: 10 }}>{this.getDistanceUnit(stop.distance).distance} {this.getDistanceUnit(stop.distance).unit}</Text>
                 </CardItem>
                 <CardItem footer>
                   <Body>
